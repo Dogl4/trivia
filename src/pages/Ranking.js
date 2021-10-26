ba5cb86178c56fc059854f4de110ef861ef9453d
Andrea <andrealh08@gmail.com>
2021-10-26 14:47:33 -0300

    Requisito 18 feito

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export class RankingPlayer extends Component {
+  constructor(props) {
+    super(props);
+    this.sendToLogin = this.sendToLogin.bind(this);
+  }
+
+  sendToLogin() {
+    const { history } = this.props;
+    history.push('/');
+  }
+
   render() {
     return (
       <div data-testid="ranking-title">
         Página de Ranking Ranqueado
+        <button
+          type="button"
+          data-testid="btn-go-home"
+          onClick={ this.sendToLogin }
+        >
+          Ir para Inicio
+        </button>
       </div>
     );
   }
 }
+
+RankingPlayer.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default RankingPlayer;
